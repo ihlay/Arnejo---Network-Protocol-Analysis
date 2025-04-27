9420afb714c9268fb82c069fb0320301ddfd8827
ihlay <arnejoelikarl@gmail.com>
2025-04-27 16:05:37 +0800

    First attempt at HTTP POST request

--- pin_script.py
+++ pin_script.py
@@ -7,8 +7,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 print("Connected!")
 
-# Try sending random text
-sock.sendall(b"Hello Server")
+# Trying to send POST request manually
+request = "POST /verify HTTP/1.1\nHost: 127.0.0.1\n\nmagicNumber=123"
+sock.sendall(request.encode())
 
 response = sock.recv(4096)
 print(response.decode())
